f8b990c255d2da04f88a6b59286b93c76ca4b1b5
Peter Corbett <peter@corbettdigital.net>
2016-09-06 11:36:05 +0000

    White lines on a white background are unexciting.

--- mtgraph.php
+++ mtgraph.php
@@ -1,6 +1,6 @@
 <?php
 header("Content-type: image/png");
 
-passthru("rrdtool graph - -a PNG DEF:count=medtent.rrd:inmedtent:AVERAGE LINE:count#FFFFFF");
+passthru("rrdtool graph - -a PNG DEF:count=medtent.rrd:inmedtent:AVERAGE LINE:count#000000");
 
 ?>
