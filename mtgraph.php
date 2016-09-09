dd149e4ef569c322182a2dd22049881928af40be
Peter Corbett <peter@corbettdigital.net>
2016-09-09 02:26:33 +0000

    Update mtgraph fetcher to be a ghetto proxy for graphite

--- mtgraph.php
+++ mtgraph.php
@@ -1,6 +1,8 @@
 <?php
 header("Content-type: image/png");
 
-passthru("rrdtool graph - -a PNG DEF:count=medtent.rrd:inmedtent:AVERAGE LINE:count#000000");
+// file_get_contents("http://10.145.67.1:8080/render/?width=384&height=128&target=trivnet.inmedtent&from=-2hours&yStep=1&lineMode=connected&hideLegend=true");
+
+passthru("curl -q \"http://10.145.67.1:8080/render/?width=384&height=128&target=trivnet.inmedtent&from=-2hours&yStep=1&lineMode=connected&hideLegend=true\"");
 
 ?>
