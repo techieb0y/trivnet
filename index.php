5c318a286b4fb11bee79c0d42dec2b20b7a830e0
Peter Corbett <peter@corbettdigital.net>
2016-09-17 13:19:50 +0000

    Get rid of magic numbers here too

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ include("head.inc");
 
 echo "<table width=\"100%\"><tr>";
 // 2 is a magic number for 'crossed finish line' enum value of status type.
-$done = query("select count(value) as done from persondata where datatype=" . $config["status"] . " and value='2'")[0]["done"];
+$done = query("select count(value) as done from persondata where datatype=" . $config["status"] . " and value='" . $config["finishstatus"] . "'")[0]["done"];
 $total = query("select count(id) as total from people")[0]["total"];
 $percent = floor(100 * ($done/$total));
 echo "<td style=\"background-color: green;\" width=\"$percent%\">$percent% Crossed Finish Line</td>";
