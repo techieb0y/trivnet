c02935dee0d0d11c69ca1f5ddb318aff2d84a976
Peter Corbett <peter@corbettdigital.net>
2016-09-18 00:27:59 +0000

    Using the same envvar, use the old-school non-AJAX message display at the top of the screen for DSTAR clients.

--- common/jquery_msg.js
+++ common/jquery_msg.js
@@ -5,11 +5,11 @@
 		$.getJSON("/trivnet/agents/messages.php?limit=25", null, function(data) { 
 		$.each( data, function(idx) {
 				if ( data[idx].dest == callsign ) {
-				$("#messages").append( '<tr class=\"msgItem tome\"><td><img class=mesgSymbol src=getAPRS.php?sym=' + data[idx].symbol + '></td><td class=msgLeft>' + data[idx].callsign + '-&gt;' + data[idx].dest + '<br>' + data[idx].tactical + '</td><td class=msgCenter> ' + data[idx].message + '</td><td class=msgRight>' + data[idx].timestamp + '</td></tr>' );
+				$("#messages").append( '<tr class=\"msgItem tome\"><td><img class=mesgSymbol src=getAPRS.php?sym=' + data[idx].symbol + '></td><td class=msgLeft>' + data[idx].callsign + ' &rarr; ' + data[idx].dest + '<br>' + data[idx].tactical + '</td><td class=msgCenter> ' + data[idx].message + '</td><td class=msgRight>' + data[idx].timestamp + '</td></tr>' );
 				} else if ( $rowNum % 2 == 0 ) {
-				$("#messages").append( '<tr class=\"msgItem evenRow\"><td><img class=mesgSymbol src=getAPRS.php?sym=' + data[idx].symbol + '></td><td class=msgLeft>' + data[idx].callsign + '-&gt;' + data[idx].dest + '<br>' + data[idx].tactical + '</td><td class=msgCenter> ' + data[idx].message + '</td><td class=msgRight>' + data[idx].timestamp + '</td></tr>' );
+				$("#messages").append( '<tr class=\"msgItem evenRow\"><td><img class=mesgSymbol src=getAPRS.php?sym=' + data[idx].symbol + '></td><td class=msgLeft>' + data[idx].callsign + ' &rarr; ' + data[idx].dest + '<br>' + data[idx].tactical + '</td><td class=msgCenter> ' + data[idx].message + '</td><td class=msgRight>' + data[idx].timestamp + '</td></tr>' );
 				} else {
-				$("#messages").append( '<tr class=\"msgItem oddRow\"><td><img class=mesgSymbol src=getAPRS.php?sym=' + data[idx].symbol + '></td><td class=msgLeft>' + data[idx].callsign + '-&gt;' + data[idx].dest + '<br>' + data[idx].tactical + '</td><td class=msgCenter> ' + data[idx].message + '</td><td class=msgRight>' + data[idx].timestamp + '</td></tr>' );
+				$("#messages").append( '<tr class=\"msgItem oddRow\"><td><img class=mesgSymbol src=getAPRS.php?sym=' + data[idx].symbol + '></td><td class=msgLeft>' + data[idx].callsign + ' &rarr; ' + data[idx].dest + '<br>' + data[idx].tactical + '</td><td class=msgCenter> ' + data[idx].message + '</td><td class=msgRight>' + data[idx].timestamp + '</td></tr>' );
 				}
 				$rowNum++;
 			})
