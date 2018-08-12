f1b94e3a5734f8afd6df5553dbaad7d5298815c5
techieb0y <peter@corbettdigital.net>
2018-08-11 21:55:54 -0500

    More CSS trickery

--- src/index.php
+++ src/index.php
@@ -24,17 +24,18 @@ echo "<table width=\"100%\">";
 $races = query("select * from race");
 foreach ( $races as $r ) {
 	echo "<tr>";
+	echo "<td>" . $r["raceid"] . "</td>";
 	$left = floor( ( 100 * ( $r["tail"] / 26.2 ) ) );
 	$right = floor( 100 - ( 100 * ( $r["head"] / 26.2 ) ) );
 	$middle = 100 - ( $left + $right );
 	if ( $left > 0 ) {
 		echo "<td width=\"$left%\" style=\"background-color: red;\">End of race past mile " . $r["tail"] . "</td>";
 	}
-	echo "<td width=\"$middle%\" style=\"background-color: white;\">\n";
-	echo "<img src=\"getAPRS.php?sym=48\" style=\"left: $left%;\">&nbsp;<img src=\"getAPRS.php?sym=59\" style=\"right: $right%;\">\n";
+	echo "<td width=\"$middle%\" style=\"background-color: grey;\">\n";
+	echo "<img src=\"getAPRS.php?sym=48\" style=\"position: absolute; left: $left%;\">&nbsp;<img src=\"getAPRS.php?sym=59\" style=\"position: absolute; right: $right%;\">\n";
 	echo "</td>\n";
 
-	echo "<td width=\"$right%\" style=\"background-color: blue;\">Lead runner past mile " . $r["head"] . "</td>";
+	echo "<td width=\"$right%\" style=\"background-color: blue; color: white;\">Lead runner past mile " . $r["head"] . "</td>";
 	echo "</tr>\n";
 }
 echo "</table><br>\n";
