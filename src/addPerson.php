68528d7695442f839fb21902e6390795882f1e5b
techieb0y <peter@corbettdigital.net>
2018-09-03 16:17:52 -0500

    Don't add a null bibnumber to manually-created people

--- src/addPerson.php
+++ src/addPerson.php
@@ -13,7 +13,7 @@ $type = $config["multidefault"];
 $r = query("SELECT MAX(id) + 1 AS nextID FROM people;");
 $next =  $r[0]["nextid"];
 echo "Got: $next";
-$s = query("BEGIN; INSERT INTO people VALUES ( '$next' ); INSERT INTO persondata VALUES ( $next, $type, null ); COMMIT;");
+$s = query("BEGIN; INSERT INTO people VALUES ( '$next' ); COMMIT;");
 
 header("Location: detail.php?id=$next");
 ?>
