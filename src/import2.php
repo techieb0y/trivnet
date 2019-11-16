0e114fd4e1fdfce9c87415a95ad9b52ccd2000a7
techieb0y <peter@corbettdigital.net>
2019-11-15 19:42:05 -0600

    Derp, use the right magic var name -- trivnet/trivnet#32

--- src/import2.php
+++ src/import2.php
@@ -5,7 +5,7 @@ require_once("include/head.inc");
 // Did they select a file to upload?
 if ( $_FILES["persondata"]["size"] > 0 ) {
 	// uploaded file, instead of entering data into textarea
-	$filename = $_SERVER["SCRIPT_PATH"] . "/jobs/" . basename($_FILES['persondata']['name']);
+	$filename = $_SERVER["SCRIPT_NAME"] . "/jobs/" . basename($_FILES['persondata']['name']);
 	move_uploaded_file($_FILES['persondata']['tmp_name'], $filename);
 	echo "Using " . filesize($filename) . " bytes of person data file contents<br>\n";
 	$use_file = 1;
