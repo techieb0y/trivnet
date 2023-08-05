ad04683742a2e3d0fd4e68d73103fccf53eba5d0
techieb0y <peter@corbettdigital.net>
2023-08-04 21:45:56 -0500

    probably mess this all up?

--- src/common/batchmode.js
+++ src/common/batchmode.js
@@ -1,13 +1,68 @@
 function makeClone() {
-	counter = document.getElementById("counter").value;
-	source = document.getElementById("template");
+
+	var newRow = document.createElement("tr");
+
+	var newIconTd = document.createElement("td");
+	var newIconImg = document.createElement("iconImg");
+	newIconImg.setAttribute("src", "images/blank.png");
+	newIconImg.setAttribute("width", "32");
+	newIconImg.setAttribute("height", "32");
+	newIconImg.setAttribute("id", "statusIcon");
+	newIconTd.appendChild(newIconImg);
+
+	var newSearchTd = document.createElement("td");
+	var newSearchInput = document.createElement("input");
+	newSearchInput.setAttribute("name", "search[]");
+	newSearchInput.setAttribute("size", "6");
+	newSearchInput.setAttribute("onBlur", "javascript:personSearch(this)");
+	newSearchInput.setAttribute("id", "search");
+	newSearchTd.appendChild(newSearchInput);
+
+	var newDisplayTd = document.createElement("td");
+	var newDisplayInput = document.createElement("input");
+	newDisplayInput.setAttribute("name", "displayName[]");
+	newDisplayInput.setAttribute("size", "50");
+	newDisplayInput.setAttribute("disabled", "disabled");
+	newDisplayInput.setAttribute("id", "displayName");
+	var newPidInput = document.createElement("input");
+	newPidInput.setAttribute("name", "personId[]");
+	newPidInput.setAttribute("type", "hidden");
+	newPidInput.setAttribute("id", "personId");
+
+	newSearchTd.appendChild(newDisplayInput);
+	newSearchTd.appendChild(newPidInput);
+
+	var newStatusTd = document.createElement("td");
+	var newStatusInput = document.createElement("input");
+	newStatusInput.setAttribute("name", "status[]");
+	newStatusInput.setAttribute("size", "50");
+	newStatusInput.setAttribute("onBlur", "javascript:personSearch(this)");
+	newStatusInput.setAttribute("id", "status");
+	newStatusTd.appendChild(newStatusInput);
+
+	var newGoTd = document.createElement("td");
+	var newGoImg = document.createElement("img");
+	newGoImg.setAttribute("name", "infoIcon");
+	newGoImg.setAttribute("onClick", "javascript:postStatus(this)");
+	newGoImg.setAttribute("src", "images/save_button.png");
+	newGoImg.style.display = "none";
+	newGoImg.setAttribute("id", "infoIcon");
+	newStatusTd.appendChild(newGoImg);
+
+	newRow.appendChild(newIconTd);
+	newRow.appendChild(newSearchTd);
+	newRow.appendChild(newDisplayTd);
+	newRow.appendChild(newStatusTd);
+	newRow.appendChild(newGoTd);
+
 	var parent = document.getElementById("outParent");
-	var clone = document.getElementById("template").cloneNode(true);
+	parent.appendChild(newRow);
+
+	counter = document.getElementById("counter").value;
 	counter++;
 	parseDOMat( clone );
 	document.getElementById("counter").value = counter;
-	clone.style.display = "inherit";
-	parent.appendChild(clone);
+
 	oneLess = counter-1;
 	document.getElementById("search" + oneLess).focus();
 } // end makeClone
