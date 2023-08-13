59a5838a6e5c43b896766c6a4cf1bc63d58be3f8
techieb0y <peter@corbettdigital.net>
2023-08-12 19:04:28 -0500

    that's all strings, not just the output ones

--- src/metrics.php
+++ src/metrics.php
@@ -9,7 +9,7 @@ require_once("include/config.inc");
 
 $status = $config["status"];
 
-$qh = "SELECT id,value FROM enumtypes WHERE datatype=${status} ORDER BY value";
+$qh = "SELECT id,value FROM enumtypes WHERE datatype={$status} ORDER BY value";
 $rh = query($qh);
 foreach ( $rh as $row ) {
         $id= $row["id"];
@@ -18,13 +18,13 @@ foreach ( $rh as $row ) {
 } // end foreach
 
 
-$q = "SELECT value, count(value) as cnt FROM persondata WHERE datatype=${status} GROUP BY value";
+$q = "SELECT value, count(value) as cnt FROM persondata WHERE datatype={$status} GROUP BY value";
 $inmed = query($q);
 
 foreach($inmed as $m) {
         $id = $m["value"];
         $label = $theBigArray[$id];
-        echo "trivnet_status{label=\"" . $label . "\"}        " . $m["cnt"]  . "\n";
+        echo "trivnet_status{label=\"{$label}\"}        {$m["cnt"]}\n";
 }
 
 $q_latch = 'select label,count(personid) as num from latchlog full outer join latchtypes on latchtypes.id=latchlog.latchid group by label;';
@@ -32,7 +32,7 @@ $q_latch = 'select label,count(personid) as num from latchlog full outer join la
 $res = pg_query( connect(), $q_latch );
 $r = array();
 while ( $z = pg_fetch_assoc($res) ) {
-	echo "latching_status{label=\"" . $z["label"] . "\"}    " . $z["num"] . "\n";
+	echo "latching_status{label=\"{$z["label"]}\"}  {$z["num"]}\n";
 } // end while
 
 ?>
