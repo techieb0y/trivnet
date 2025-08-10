2fa1d2ad0dab2afb5f696b8c8631a24f3a8e337c
Peter Corbett <peter@corbettdigital.net>
2025-08-09 20:34:37 -0500

    Brute-force fix for trivnet/trivnet#21

--- src/message_send.php
+++ src/message_send.php
@@ -13,9 +13,12 @@ if ( isset($_POST["sendto"]) ) {
 } // end if
 
 $from = $_SESSION["callsign"];
+$mytac = $_SESSION["tactical"];
+$mysym = $_SESSION["symbol"];
+
 $text = pg_escape_string(connect(), $_POST["text"]);
 
-$q = "INSERT INTO messages VALUES ( " . time() . ", '$from', '$text', '$to' )";
+$q = "INSERT INTO messages VALUES ( " . time() . ", '$from', '$mysym', '$mytac', '$text', '$to' )";
 $r = query($q);
 
 header("Location: messaging.php");
