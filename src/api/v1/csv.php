0dbfe00a4a1bb553fe0ab0434359d5e818aca8ae
Peter Corbett <peter@corbettdigital.net>
2025-08-25 19:07:30 -0500

    testing stub for a public-ish API

---
+++ src/api/v1/csv.php
@@ -0,0 +1,26 @@
+<?php 
+
+// Test page for handling CSV uploads
+
+require_once("include/db_ops.inc");
+require_once("include/config.inc");
+
+header("Content-Type: text/plain")
+
+if ( isset( $_SERVER["CONTENT_TYPE"] ) ) {
+    if ( "text/csv" == $_SERVER["CONTENT_TYPE"] ) {
+        echo "OK"
+
+        $postbody = stream_get_contents(STDIN);
+        $data = fgetcsv($postbody);
+
+        print_r($data);
+
+    } else {
+        header("HTTP/415 Unsupported Media Type");
+        echo "Unexpected conent type"
+    }
+} else {
+    header("HTTP/400 Bad Request");
+    echo "Missig content type"
+}
