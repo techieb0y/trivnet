c76a94c40228461bf70abb9c0cd8d86dd8cb7cd1
Peter Corbett <peter@corbettdigital.net>
2025-08-26 20:02:49 -0500

    how's it parsing this?

--- src/api/v1/csv.php
+++ src/api/v1/csv.php
@@ -1,7 +1,5 @@
 <?php 
 
-ini_set("auto_detect_line_endings", true);
-
 // Test page for handling CSV uploads
 
 require_once("../../include/db_ops.inc");
@@ -11,13 +9,11 @@ header("Content-Type: text/plain");
 
 if ( isset( $_SERVER["CONTENT_TYPE"] ) ) {
     if ( "text/csv" == $_SERVER["CONTENT_TYPE"] ) {
-        echo "OK\n";
-
         $file = fopen("php://input", "r");
         while ( $row = fgetcsv($file) ) {
+            echo "Got row: ${row}";
             $data[] = $row;
         }
-
         print_r($data);
     } else {
         http_response_code(415);
