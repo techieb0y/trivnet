4e9e4fa221b5c86f4f6c102b12c0cb6192711f5a
Peter Corbett <peter@corbettdigital.net>
2025-08-30 15:56:50 -0500

    Stuff things into the async table

--- src/api/v1/csv.php
+++ src/api/v1/csv.php
@@ -8,15 +8,35 @@ require_once("../../include/config.inc");
 header("Content-Type: text/plain");
 
 if ( isset( $_SERVER["CONTENT_TYPE"] ) ) {
+    // Save CSV input into a file
     if ( "text/csv" == $_SERVER["CONTENT_TYPE"] ) {
+        $tmp = tmpfile() or die("Unable to open tempfile.");
+
         $file = fopen("php://input", "r");
         while ( $row = trim(fgets($file)) ) {
-            echo "Got row: {$row}\n";
-            if ( strlen($row) > 0 ) {
-                $data[] = $row;
-            }
+            // echo "Got row: {$row}\n";
+            fwrite($tmp, $row . "\n");
         }
-        print_r($data);
+        fflush($tmp);
+
+        // Set up the async file in the right place
+        $q_id = "SELECT nextval('async_jobid_seq') AS jobid";
+        $r_id = query($q_id);
+        $jobid = $r_id[0]["jobid"];
+
+        $jobfile = "../../jobs/API-" . $jobid;
+        $fh = fopen($jobfile, "w+");
+        fseek($tmp, 0);
+        stream_copy_to_stream($tmp, $fh);
+        fclose($fh);
+
+        // CREATE TABLE async ( jobid serial, filename varchar(64) not null, callsign varchar(8) not null, searchtype int REFERENCES datatypes(typeid), updatetype int, data varchar(255) not null, state int DEFAULT 0, progress int DEFAULT 0, timestamp int NOT NULL);
+        $q_submit = "INSERT INTO async VALUES ( $jobid, '$jobfile', 'API', '$config["multidefault"]', '$config["message"]', 'Runner crossed finish line', 1, 0, '" . time() . "');";
+	    $r_submit = query($q_submit);
+
+        $q_submit = "INSERT INTO async VALUES ( $jobid, '$jobfile', 'API', '$config["multidefault"]', '999', '1', 1, 0, '" . time() . "');";
+	    $r_submit = query($q_submit);
+
     } else {
         http_response_code(415);
         echo "Unexpected conent type\n";
