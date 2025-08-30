a07701f10eef1d96ba7f8ef1519100ad3c25bd75
Peter Corbett <peter@corbettdigital.net>
2025-08-30 17:13:55 -0500

    make dupes of latching status fatal to the whole transaction

--- src/async.php
+++ src/async.php
@@ -301,7 +301,8 @@ function runJob($jobId) {
 						$latchData[] = $z;
 					} // end while
 
-					$q .= "INSERT INTO latchlog VALUES ('$personid', '$data') on conflict do nothing;\n";
+					// $q .= "INSERT INTO latchlog VALUES ('$personid', '$data') on conflict do nothing;\n";
+					$q .= "INSERT INTO latchlog VALUES ('$personid', '$data');\n";
 					$text = "Set latch " . $latchData[0]["label"];
 					$q .= "INSERT INTO updatesequence VALUES ('$personid', '$now', '$callsign/$tac [B]', $msg, '$text');\n";
 					$q .= "COMMIT;\n";
