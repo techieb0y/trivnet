ea17e86e417df8ff89439a6a7dd0e51fef4fa0d8
Peter Corbett <peter@corbettdigital.net>
2025-10-06 20:54:59 -0500

    Still not ideal

--- src/common/jquery.js
+++ src/common/jquery.js
@@ -7,11 +7,11 @@
 			$.each( data, function(idx) {
 				ts = new Date( data[idx].timestamp * 1000 );
 				if ( data[idx].dest == callsign ) {
-				$("#messages").append( '<tr class=\"msgItem tome\"><td><img class=mesgSymbol src=symbol/' + data[idx].symbol + '></td><td class=msgLeft>' + data[idx].callsign + ' &rarr; ' + data[idx].dest + '<br>' + data[idx].tactical + '</td><td class=msgCenter> ' + data[idx].message + '</td><td class=msgRight>' + ts.toString() + '</td></tr>' );
+				$("#messages").append( '<tr class=\"msgItem tome\"><td><img class=mesgSymbol src=symbol/' + data[idx].symbol + '></td><td class=msgLeft>' + data[idx].callsign + ' &rarr; ' + data[idx].dest + '<br>' + data[idx].tactical + '</td><td class=msgCenter> ' + data[idx].message + '</td><td class=msgRight>' + ts.toLocaleString() + '</td></tr>' );
 				} else if ( $rowNum % 2 == 0 ) {
-				$("#messages").append( '<tr class=\"msgItem evenRow\"><td><img class=mesgSymbol src=symbol/' + data[idx].symbol + '></td><td class=msgLeft>' + data[idx].callsign + ' &rarr; ' + data[idx].dest + '<br>' + data[idx].tactical + '</td><td class=msgCenter> ' + data[idx].message + '</td><td class=msgRight>' + ts.toString() + '</td></tr>' );
+				$("#messages").append( '<tr class=\"msgItem evenRow\"><td><img class=mesgSymbol src=symbol/' + data[idx].symbol + '></td><td class=msgLeft>' + data[idx].callsign + ' &rarr; ' + data[idx].dest + '<br>' + data[idx].tactical + '</td><td class=msgCenter> ' + data[idx].message + '</td><td class=msgRight>' + ts.toLocaleString() + '</td></tr>' );
 				} else {
-				$("#messages").append( '<tr class=\"msgItem oddRow\"><td><img class=mesgSymbol src=symbol/' + data[idx].symbol + '></td><td class=msgLeft>' + data[idx].callsign + ' &rarr; ' + data[idx].dest + '<br>' + data[idx].tactical + '</td><td class=msgCenter> ' + data[idx].message + '</td><td class=msgRight>' + ts.toString() + '</td></tr>' );
+				$("#messages").append( '<tr class=\"msgItem oddRow\"><td><img class=mesgSymbol src=symbol/' + data[idx].symbol + '></td><td class=msgLeft>' + data[idx].callsign + ' &rarr; ' + data[idx].dest + '<br>' + data[idx].tactical + '</td><td class=msgCenter> ' + data[idx].message + '</td><td class=msgRight>' + ts.toLocaleString() + '</td></tr>' );
 				}
 				$rowNum++;
 			})
--- src/common/jquery_msg.js
+++ src/common/jquery_msg.js
@@ -4,15 +4,15 @@
 		$rowNum = 0;
 		$.getJSON("/agents/messages.php?limit=18", null, function(data) { 
 		$.each( data, function(idx) {
-				if ( data[idx].dest == callsign ) {
 				ts = new Date( data[idx].timestamp * 1000 );
-				$("#messages").append( '<tr class=\"msgItem tome\"><td><img class=mesgSymbol src=symbol/' + data[idx].symbol + '></td><td class=msgLeft>' + data[idx].callsign + ' &rarr; ' + data[idx].dest + '<br>' + data[idx].tactical + '</td><td class=msgCenter> ' + data[idx].message + '</td><td class=msgRight>' + ts.toString() + '</td></tr>' );
+				if ( data[idx].dest == callsign ) {
+  				$("#messages").append( '<tr class=\"msgItem tome\"><td><img class=mesgSymbol src=symbol/' + data[idx].symbol + '></td><td class=msgLeft>' + data[idx].callsign + ' &rarr; ' + data[idx].dest + '<br>' + data[idx].tactical + '</td><td class=msgCenter> ' + data[idx].message + '</td><td class=msgRight>' + ts.toLocaleString() + '</td></tr>' );
 				} else if ( data[idx].callsign == callsign) {
-				$("#messages").append( '<tr class=\"msgItem fromme\"><td><img class=mesgSymbol src=symbol/' + data[idx].symbol + '></td><td class=msgLeft>' + data[idx].callsign + ' &rarr; ' + data[idx].dest + '<br>' + data[idx].tactical + '</td><td class=msgCenter> ' + data[idx].message + '</td><td class=msgRight>' + ts.toString() + '</td></tr>' );
+				$("#messages").append( '<tr class=\"msgItem fromme\"><td><img class=mesgSymbol src=symbol/' + data[idx].symbol + '></td><td class=msgLeft>' + data[idx].callsign + ' &rarr; ' + data[idx].dest + '<br>' + data[idx].tactical + '</td><td class=msgCenter> ' + data[idx].message + '</td><td class=msgRight>' + ts.toLocaleString() + '</td></tr>' );
 				} else if ( $rowNum % 2 == 0 ) {
-				$("#messages").append( '<tr class=\"msgItem evenRow\"><td><img class=mesgSymbol src=symbol/' + data[idx].symbol + '></td><td class=msgLeft>' + data[idx].callsign + ' &rarr; ' + data[idx].dest + '<br>' + data[idx].tactical + '</td><td class=msgCenter> ' + data[idx].message + '</td><td class=msgRight>' + ts.toString() + '</td></tr>' );
+				$("#messages").append( '<tr class=\"msgItem evenRow\"><td><img class=mesgSymbol src=symbol/' + data[idx].symbol + '></td><td class=msgLeft>' + data[idx].callsign + ' &rarr; ' + data[idx].dest + '<br>' + data[idx].tactical + '</td><td class=msgCenter> ' + data[idx].message + '</td><td class=msgRight>' + ts.toLocaleString() + '</td></tr>' );
 				} else {
-				$("#messages").append( '<tr class=\"msgItem oddRow\"><td><img class=mesgSymbol src=symbol/' + data[idx].symbol + '></td><td class=msgLeft>' + data[idx].callsign + ' &rarr; ' + data[idx].dest + '<br>' + data[idx].tactical + '</td><td class=msgCenter> ' + data[idx].message + '</td><td class=msgRight>' + ts.toString() + '</td></tr>' );
+				$("#messages").append( '<tr class=\"msgItem oddRow\"><td><img class=mesgSymbol src=symbol/' + data[idx].symbol + '></td><td class=msgLeft>' + data[idx].callsign + ' &rarr; ' + data[idx].dest + '<br>' + data[idx].tactical + '</td><td class=msgCenter> ' + data[idx].message + '</td><td class=msgRight>' + ts.toLocaleString() + '</td></tr>' );
 				}
 				$rowNum++;
 			})
