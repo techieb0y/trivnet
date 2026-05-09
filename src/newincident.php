d8b3c910b134bf694622452f247ae6f846a66264
Peter Corbett <peter@corbettdigital.net>
2026-05-09 17:25:39 -0500

    debug

--- src/newincident.php
+++ src/newincident.php
@@ -6,9 +6,14 @@ include("include/db_ops.inc");
 $r = query("SELECT COALESCE((SELECT MAX(id) FROM incidents), 1) as next;");
 $next =  $r[0]["next"];
 
+error_log("Next incident ID is {$next}");
+
 $title = pg_escape_string(connect(), $_POST["title"]);
 $text = "Incident \"{$title}\" opened";
-$q = "BEGIN; INSERT INTO incidents VALUES ( '{$next}', '{$title}', 'open' ); INSERT INTO incidentsequence VALUES ({$next}, extract(epoch from NOW()), '{$text}' ); COMMIT;";
+$q = "BEGIN; INSERT INTO incidents VALUES ( '{$next}', '{$title}', 'open' ); INSERT INTO incidentsequence VALUES ('{$next}', extract(epoch from NOW()), '{$text}' ); COMMIT;";
+
+error_log($q);
+
 $r = query($q);
 
 header("Location: incident.php?id={$next}");
