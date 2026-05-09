6638a6bfb338e28bc66d8953a6e00b103ad94a0a
Peter Corbett <peter@corbettdigital.net>
2026-05-09 16:56:33 -0500

    Handle the startup case where there are no incidents yet.

--- src/newincident.php
+++ src/newincident.php
@@ -6,6 +6,8 @@ include("include/db_ops.inc");
 $r = query("SELECT MAX(id) + 1 AS next FROM incidents;");
 $next =  $r[0]["next"];
 
+if ( ! $next > 0 ) { $next = 1; }
+
 $title = pg_escape_string(connect(), $_POST["title"]);
 $text = "Incident \"{$title}\" opened";
 $q = "BEGIN; INSERT INTO incidents VALUES ( '$next', '$title', 'open' ); INSERT INTO incidentsequence VALUES ({$next}, extract(epoch from NOW()), '{$text}' ); COMMIT;";
