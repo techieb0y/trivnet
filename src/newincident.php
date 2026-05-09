74acc07b59638c35f744d33b4a72e43ad06e25c9
Peter Corbett <peter@corbettdigital.net>
2026-05-09 17:14:30 -0500

    Better SQL-native fix for bootstrap case

--- src/newincident.php
+++ src/newincident.php
@@ -3,14 +3,12 @@ session_start();
 
 include("include/db_ops.inc");
 
-$r = query("SELECT MAX(id) + 1 AS next FROM incidents;");
+$r = query("SELECT COALESCE((SELECT MAX(id) FROM incidents), 1) as next;");
 $next =  $r[0]["next"];
-
-if ( ! $next > 0 ) { $next = 1; }
 
 $title = pg_escape_string(connect(), $_POST["title"]);
 $text = "Incident \"{$title}\" opened";
-$q = "BEGIN; INSERT INTO incidents VALUES ( '$next', '$title', 'open' ); INSERT INTO incidentsequence VALUES ({$next}, extract(epoch from NOW()), '{$text}' ); COMMIT;";
+$q = "BEGIN; INSERT INTO incidents VALUES ( '{$next}', '{$title}', 'open' ); INSERT INTO incidentsequence VALUES ({$next}, extract(epoch from NOW()), '{$text}' ); COMMIT;";
 $r = query($q);
 
 header("Location: incident.php?id={$next}");
